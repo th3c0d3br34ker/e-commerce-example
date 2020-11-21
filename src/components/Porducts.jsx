b985d9b0a4dc531f1217f5f423afa6fdf3e9c993
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-11-22 02:13:31 +0530

    🔧 Update Products

---
+++ src/components/Porducts.jsx
@@ -0,0 +1,35 @@
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import ProductCard from "./ProductCard";
+import { data } from "../utils/data";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 800,
+    maxHeight: "100vh",
+    padding: 0,
+    margin: 0,
+  },
+});
+
+const Products = ({ AddItem }) => {
+  const classes = useStyles();
+
+  //Extract the Photoa from the data.
+  const { photos } = data;
+
+  return (
+    <Container className={classes.root}>
+      <Box flexWrap="wrap" display="flex" p={1} m={1}>
+        {photos.map((photoInfo) => (
+          <Box key={photoInfo.id} p={1} m={1}>
+            <ProductCard photoInfo={photoInfo} AddItem={AddItem} />
+          </Box>
+        ))}
+      </Box>
+    </Container>
+  );
+};
+
+export default Products;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,116 @@
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+//  Define the styles for the Card.
+//  Took a while, I think its worth it. 😬
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 350,
+    margin: "auto 0",
+  },
+  media: {
+    borderRadius: "8px",
+  },
+  cardBody: {
+    borderRadius: "8px",
+  },
+  by: {
+    color: "grey",
+    fontSize: "13px",
+    padding: "8px 8px 0 0",
+    float: "right",
+    textDecoration: "none",
+  },
+  info: {
+    padding: "8px",
+    marginTop: "16px",
+  },
+  infoData: {
+    padding: "8px",
+    color: "rgb(0 0 0 / 0.8)",
+  },
+  button: {
+    // "border": "1px solid black",
+    "backgroundColor": "grey",
+    "color": "white",
+    "&:hover": {
+      backgroundColor: "white",
+      color: "black",
+    },
+  },
+});
+
+const ProductCard = ({ photoInfo, AddItem }) => {
+  const classes = useStyles();
+  const {
+    id,
+    photographer,
+    photographer_url,
+    src,
+    width,
+    height,
+    url,
+  } = photoInfo;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent className={classes.cardBody}>
+        <CardMedia
+          component="img"
+          className={classes.media}
+          image={src.medium}
+        />
+        <Typography
+          className={classes.by}
+          component="a"
+          href={photographer_url}
+        >
+          by {photographer}
+        </Typography>
+
+        <Typography component="h1" variant="h6" className={classes.info}>
+          More Info
+        </Typography>
+
+        <Typography
+          variant="body1"
+          className={classes.infoData}
+          color="textSecondary"
+        >
+          id: #{id}
+          <br />
+          Original Resolution: {width}x{height}
+        </Typography>
+
+        <CardActions>
+          <Button
+            className={classes.button}
+            size="small"
+            onClick={() => {
+              AddItem(photoInfo);
+            }}
+            variant="outlined"
+          >
+            Add Item
+          </Button>
+          <Button
+            className={classes.button}
+            size="small"
+            target="_black"
+            href={url}
+            variant="outlined"
+          >
+            See Original
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
