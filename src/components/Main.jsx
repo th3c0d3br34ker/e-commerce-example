effb78105ac75d2c14e07fb53d26b2b43c88ba0e
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-11-22 15:59:33 +0530

    🔧 Added data persistence

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import BuyPage from "./BuyPage/BuyPage";
 import Products from "./ProductsPage/ProductsPage";
 
 const Main = () => {
   const [cart, setCart] = useState([]);
+
+  // useEffect runs once when component renders.
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("e-commerce-app-data"));
+    if (data) {
+      setCart(data);
+    }
+  }, []);
+
+  // useEffect runs everytime the value of Cart changes.
+  useEffect(() => {
+    localStorage.setItem("e-commerce-app-data", JSON.stringify(cart));
+  }, [cart]);
 
   // AddItem function to handle Add Item Event.
   // Takes the item to be added as a parameter.
